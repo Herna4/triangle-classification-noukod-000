6997807a41694d571470ece709f2a5dcfc17effc
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 19:09:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,8 @@ class Triangle
 
    def valid?
     sum_one_two = @triangle_sides_1[0] + @triangle_sides_2[1]
-    sum_one_three = @triangle_sides_1[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+    sum_one_three = @triangle_sides_1[0] + @triangle_sides_3[2]
+    sum_two_three = @triangle_sides_2[1] + @triangle_sides_3[2]
 
     if (@triangle_sides.none? {|side| side <= 0}) &&
       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
