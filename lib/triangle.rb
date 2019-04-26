b13e2487474fd92798bf6c65070e5b49f522c692
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 19:11:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,34 +8,25 @@ class Triangle
       @triangle_sides_3 = triangle_sides_3
     end
 
-   def valid?
-    sum_one_two = @triangle_sides_1[0] + @triangle_sides_2[1]
-    sum_one_three = @triangle_sides_1[0] + @triangle_sides_3[2]
-    sum_two_three = @triangle_sides_2[1] + @triangle_sides_3[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
-  end
-
-  def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
+
   end
+
 end
 
 class TriangleError < StandardError
-
+  # triangle error code
 end
