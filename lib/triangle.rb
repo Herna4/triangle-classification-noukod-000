e767680b4853239dc5eb434dafdfdfcaf81f09fa
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 19:14:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     elsif (@triangle_sides_1 + @triangle_sides_2 <= @triangle_sides_3) || (@triangle_sides_1 + @triangle_sides_3 <= @triangle_sides_2) || (@triangle_sides_2+@triangle_sides_3 <= @triangle_sides_1)
       raise TriangleError
     else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
+      if (@triangle_sides_1 == @triangle_sides_2) && (@triangle_sides_2 == @triangle_sides_3)
         :equilateral
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
