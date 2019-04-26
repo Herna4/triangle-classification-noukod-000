7c20e8e865ec3dfbc202a3de20af4e4443847361
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 18:48:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,78 +1,40 @@
+require 'pry'
 class Triangle
-  # write code here
-  
   def initialize(side_1, side_2, side_3)
-    @triangle << side_1
-    @triangle << side_2
-    @triangle << side_3 
-    @triangle = []
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
   end
 
-
   def valid?
-    sum_one_two = @triangle[0] + @triangle[1]
-    sum_one_three = @triangle[0] + @triangle[2]
-    sum_two_three = @triangle[1] + @triangle[2]
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
 
-    if (@triangle.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle[2] && sum_one_three > @triangle[1] && sum_two_three > @triangle[0])
-       true
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+      return true
     else
-       false
+      return false
     end
   end
 
   def kind
     if valid?
-      if @triangle.uniq.length == 1
-        :equilateral
-       elsif @triangle.uniq.length == 2
-        :isosceles
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
       else
-        :scalene
+        return :scalene
       end
-      else
-        raise TriangleError
-      end
+    else
+      raise TriangleError
     end
-   
+  end
 end
 
 class TriangleError < StandardError
 
 end
-
-
-
-
-#   def valid?
-#     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-#     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-#     sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-#     if (@triangle_sides.none? {|side| side <= 0}) &&
-#       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-#       return true
-#     else
-#       return false
-#     end
-#   end
-
-#   def kind
-#     if valid?
-#       if @triangle_sides.uniq.length == 1
-#         return :equilateral
-#       elsif @triangle_sides.uniq.length == 2
-#         return :isosceles
-#       else
-#         return :scalene
-#       end
-#     else
-#       raise TriangleError
-#     end
-#   end
-# end
-
-# class TriangleError < StandardError
-
-# end
