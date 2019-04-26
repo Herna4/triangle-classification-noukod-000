7116c524bcf24f0caa7a5f7b120de88374968bc2
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 18:24:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,11 @@
 class Triangle
   # write code here
   
-  def initialize(side_1, side_2, side_3)
-    @equilateral << side_1 
-    @isosceles << side_2 
-    @scalene << side_3
-    @triangle = []
-  end
+   def initialize(side_1, side_2, side_3)
+#     @triangle_sides = []
+#     @triangle_sides << side_1
+#     @triangle_sides << side_2
+#     
  
   def kind 
   end
