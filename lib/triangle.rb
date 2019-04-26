4b805bd6b85a963a04786e0d3ffa3f246b7b5be7
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 18:30:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,12 @@ class Triangle
   end
   
 end
-# def initialize(side_1, side_2, side_3)
-#     @triangle_sides = []
-#     @triangle_sides << side_1
-#     @triangle_sides << side_2
-#     @triangle_sides << side_3
-#   end
+def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+  end
 
 #   def valid?
 #     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
