f71cfde0aa287c071a7c67bddc44b6e864f0bf36
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 18:40:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,19 +22,19 @@ class Triangle
     end
   end
 end
-#   def kind
-#     if valid?
-#       if @triangle_sides.uniq.length == 1
-#         return :equilateral
-#       elsif @triangle_sides.uniq.length == 2
-#         return :isosceles
-#       else
-#         return :scalene
-#       end
-#     else
-#       raise TriangleError
-#     end
-#   end
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
 # end
 
 class TriangleError < StandardError
